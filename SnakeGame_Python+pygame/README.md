553b474235f623633d996b20a9109dd36ab81c77
RenanSN <renan.nascimento98@outlook.com>
2020-02-01 04:25:02 -0300

    jogodacobrinhaV2

--- SnakeGame_Python+pygame/README.md
+++ SnakeGame_Python+pygame/README.md
@@ -3,10 +3,11 @@
   <img alt="GitHub stars" src="https://img.shields.io/badge/PYTHON-v3.7-blue?style=for-the-badge">
  </a>
 
-# SnakeGame_Python
+# SnakeGame_Python <img src="https://img.icons8.com/dusk/36/000000/python.png">
 ```markdown
 jogo da cobrinha em python usando a lib pygame
 ```
+![](capa.png)
 
 #### Ferramentas e Tecnologias: <br>
 [**Python**](https://www.python.org/downloads/windows/) 3.7.0 (64-bit)
@@ -15,5 +16,6 @@ jogo da cobrinha em python usando a lib pygame
 [**pygame**](https://www.pygame.org/news) 1.9.6
 - **Instalação** <code> pip install pygame </code>
 
-Dentro da pasta do projeto execute pelo terminal <code>python main.py</code>
+Dentro da pasta do projeto execute pelo terminal <code>python main.py</code>, pra executar o jogo.
 
+![](game_over.png)
--- SnakeGame_Python+pygame/main.py
+++ SnakeGame_Python+pygame/main.py
@@ -2,8 +2,8 @@ import pygame, random
 from pygame.locals import *
 
 def on_grid_random():
-    x = random.randint(0,590)
-    y = random.randint(0,590)
+    x = random.randint(0,500)
+    y = random.randint(0,500)
     return (x//10 * 10, y//10 * 10)
 
 def collision(c1, c2):
@@ -29,48 +29,97 @@ apple = pygame.Surface((10,10))
 apple.fill((105,47,45)) # cor da maçã
 
 #direção que inicia
-my_direction = LEFT
+my_direction = RIGHT
 
 clock = pygame.time.Clock()
 
-while True:
-    clock.tick(5) # Velocidade
+# Pontuação
+font = pygame.font.Font('freesansbold.ttf', 18)
+score = 0
+
+game_over = False
+
+while not game_over:
+    clock.tick(1) # Velocidade
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+            exit()
 
         if event.type == KEYDOWN:
-            if event.key == K_UP:
+            if event.key == K_UP and my_direction != DOWN:
                 my_direction = UP
-            if event.key == K_DOWN:
+            if event.key == K_DOWN and my_direction != UP:
                 my_direction = DOWN
-            if event.key == K_RIGHT:
+            if event.key == K_RIGHT and my_direction != LEFT:
                 my_direction = RIGHT
-            if event.key == K_LEFT:
+            if event.key == K_LEFT and my_direction != RIGHT:
                 my_direction = LEFT
 
     #colisão com a maçã
     if collision(snake[0], apple_pos):
         apple_pos = on_grid_random()
         snake.append((0,0))
+        score = score + 1
+
+    # Verifica se a cobra colidiu com as boras
+    if snake[0][0] == 500 or snake[0][1] == 500 or snake[0][0] < 0 or snake[0][1] < 0:
+        game_over = True
+        break
+
+    # Verifique se a cobra atingiu a si mesma
+    for i in range(1, len(snake) - 1):
+        if snake[0][0] == snake[i][0] and snake[0][1] == snake[i][1]:
+            game_over = True
+            break
+
+    if game_over:
+        break
 
     # movimentação da cabeça e sobre posição da cobra
-    for i in range(len(snake) -1 , 0, -1):
-        snake[i] = (snake[i-1][0], snake[i-1][1])
+    #for i in range(len(snake) - 1, 0, -1):
+    #    snake[i] = (snake[i-1][0], snake[i-1][1])
 
-    # Movimentação
+    snake.pop()
+
+    # Actually make the snake move.
     if my_direction == UP:
-        snake[0] = (snake[0][0], snake[0][1] - 10)
+        snake.insert(0, (snake[0][0], snake[0][1] - 10))
     if my_direction == DOWN:
-        snake[0] = (snake[0][0], snake[0][1] + 10)
+        snake.insert(0, (snake[0][0], snake[0][1] + 10))
     if my_direction == RIGHT:
-        snake[0] = (snake[0][0] + 10, snake[0][1])
+        snake.insert(0, (snake[0][0] + 10, snake[0][1]))
     if my_direction == LEFT:
-        snake[0] = (snake[0][0] - 10, snake[0][1])
+        snake.insert(0, (snake[0][0] - 10, snake[0][1]))
 
     screen.fill((39,174,96)) # Cor de fundo
     screen.blit(apple,apple_pos)
+
+    for x in range(0, 500, 10): # Desenhar linhas verticais
+        pygame.draw.line(screen, (47, 54, 64), (x, 0), (x, 500))
+    for y in range(0, 500, 10): # Desenhar linhas horizontal
+        pygame.draw.line(screen, (47, 54, 64), (0, y), (500, y))
+    
+    score_font = font.render('Score: %s' % (score), True, (0,0,0))
+    score_rect = score_font.get_rect()
+    score_rect.topleft = (500 - 120, 15)
+    screen.blit(score_font, score_rect)
+
     for pos in snake:
         screen.blit(snake_skin,pos)
 
     pygame.display.update()
+
+while True:
+    game_over_font = pygame.font.Font('freesansbold.ttf', 75)
+    game_over_screen = game_over_font.render('Game Over', True, (232, 65, 24))
+    game_over_rect = game_over_screen.get_rect()
+    game_over_rect.midtop = (500 / 2, 170)
+    screen.blit(game_over_screen, game_over_rect)
+    pygame.display.update()
+    pygame.time.wait(500)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
